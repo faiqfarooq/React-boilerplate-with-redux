1202ebb3874256dcc2a4beb009540dc3a66b80c4
faiq farooq <faiqfarooq777@gmail.com>
2023-03-10 09:43:37 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+<h1>Hello its me Faiq Farooq</h1>
+<h1>Lets develop a React App ...</h1>
+
+
+    </React.Fragment>
+  )
+}
+
+export default App;
